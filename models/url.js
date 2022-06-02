3a72b67cd9e3839b2d975cff9d3aed35ef7d587a
AdrieneTZ <tznali17@gmail.com>
2022-06-02 11:57:19 +0800

    feat: add function: shorten raw url and get result on shortURL page

--- models/url.js
+++ models/url.js
@@ -1,17 +1,15 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-
 const urlSchema = new Schema({
-  rawUrl: {
+  rawURL: {
     type: String,
     required: true
   },
-  shortenUrl: {
+  shortURL: {
     type: String,
     required: true
   }
 })
 
-// export schema and named it 'Todo'
 module.exports = mongoose.model('Url', urlSchema)
--- routes/modules/shortURL.js
+++ routes/modules/shortURL.js
@@ -6,11 +6,26 @@ const URL = require('../../models/url')
 
 // get POST from index.hbs
 router.post('/', (req, res) => {
-  const rawURL = req.body.rawURL
-  console.log(rawURL)
+  const { rawURL } = req.body
 
-  const shortURL = shortenURL(rawURL)
-  res.render('shortURL', { shortURL })
+  URL.findOne({ rawURL }, (err, urlObj) => {
+    if (err) res.send('Network Problem')
+
+    if (urlObj) {
+      // If data is in database, render shortURL.hbs
+      res.render('shortURL', { shortURL: urlObj.shortURL })
+    } else {
+      const shortURL = shortenURL(rawURL)
+      // If data is not in database, create new data
+      URL.create({
+        rawURL,
+        shortURL
+      }).then(() => {
+        // As data is created, render shortURL.hbs
+        res.render('shortURL', { shortURL })
+      })
+    }
+  })
 })
 
 module.exports = router
